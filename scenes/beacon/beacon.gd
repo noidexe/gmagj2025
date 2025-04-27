b1667b68bb5e98867fc6e47022f4b2fb9af30fec
Lisandro Lorea <lisandrolorea@gmail.com>
2025-04-27 03:07:22 -0300

    Add sound trigger to beacons

--- scenes/beacon/beacon.gd
+++ scenes/beacon/beacon.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	light.hide()
 	assert(get_node_or_null("Camera3D"))
 	assert(get_node_or_null("model"))
+	$trigger.global_position = get_node("model").global_position
 
 func focus():
 	(get_node("Camera3D") as Camera3D).make_current()
@@ -14,3 +15,6 @@ func focus():
 	
 func unfocus():
 	light.hide()
+
+func _on_trigger_body_entered(body: Node3D) -> void:
+	$sfx.play()
