60ae532618c2f3b74a07965cfd17b6f6a7234cb6
Lisandro Lorea <lisandrolorea@gmail.com>
2025-05-05 20:45:11 -0300

    Increase beacon radius

--- scenes/beacon/beacon.gd
+++ scenes/beacon/beacon.gd
@@ -1,13 +1,17 @@
+@tool
 extends Marker3D
 class_name Beacon
 
 @onready var light = $OmniLight3D
+@onready var backoff : Timer = $backoff
 
 func _ready() -> void:
-	light.hide()
-	assert(get_node_or_null("Camera3D"))
-	assert(get_node_or_null("model"))
-	$trigger.global_position = get_node("model").global_position
+	if not Engine.is_editor_hint():
+		light.hide()
+		assert(get_node_or_null("Camera3D"))
+		assert(get_node_or_null("model"))
+	if get_node_or_null("model"):
+		$trigger.global_position = get_node("model").global_position
 
 func focus():
 	(get_node("Camera3D") as Camera3D).make_current()
@@ -17,4 +21,8 @@ func unfocus():
 	light.hide()
 
 func _on_trigger_body_entered(body: Node3D) -> void:
-	$sfx.play()
+	if Engine.is_editor_hint():
+		return
+	if backoff.is_stopped():
+		$sfx.play()
+		backoff.start()
